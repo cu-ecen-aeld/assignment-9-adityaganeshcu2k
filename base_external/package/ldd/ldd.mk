9534d586a6281f1f0e467ff1eb34f7e0349d83ce
aditya <aditya@Ubuntu.myguest.virtualbox.org>
2026-03-01 03:22:36 -0700

    Fixed the permission issue for the module_load and scull_load scripts
    Signed-off-by: aditya <aditya@Ubuntu.myguest.virtualbox.org>

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -6,11 +6,11 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-LDD_VERSION = '5c3cae6ddc96b8645dfa6f6bc4ddbba08aae8789'
+LDD_VERSION = 5c3cae6ddc96b8645dfa6f6bc4ddbba08aae8789
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-adityaganeshcu2k.git'
+LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-adityaganeshcu2k.git
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = NO
 
